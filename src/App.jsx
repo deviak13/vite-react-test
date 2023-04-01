dca58f61c1d49b603a93f938d996eda8a75ea981
kj <k.jehser@gmail.com>
2023-04-01 21:43:50 +0200

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+const BASE_URL = "https://mythic-benefit-371318.rj.r.appspot.com/";
+
+function App() {
+  const [msg, setMsg] = useState("");
+
+  useEffect(() => {
+    fetch(BASE_URL)
+      .then((response) => {
+        const json = response.json();
+        console.log(json);
+        if (response.ok) {
+          return json;
+        }
+        throw response;
+      })
+      .then((data) => {
+        setMsg(data);
+      })
+      .catch((error) => {
+        console.log(error);
+        alert(error);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>{msg}</h1>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  );
+}
+
+export default App;
